9e4ec0d1babb43404e4afb6130ab099cae38caa0
Steven Herrera <stevenherrera70396@gmail.com>
2020-03-10 19:15:33 -0700

    l

--- recursion/recursion/recursion/recursion.cpp
+++ recursion/recursion/recursion/recursion.cpp
@@ -3,32 +3,29 @@
 
 using namespace std;
 
+void printLevels(int level) {
+    if (level < 4) {
+
+    }
+}
+
 void rearrangements(string first, string second, int count) {
-    string temp = "";
-    int length = first.length();
-    if (count == first.length() - 1) {
-        cout << first << endl;
-    } else {
-        for (int i = count; i < first.length(); i++) {
-            temp = first[count];
-            if (count <= first.length()) {
-                if (i == length - 1) {
-                    temp = first[i];
-                    first.erase(i, 1);
-                    first.insert(count, 1, temp[0]);
-                } else {
-                    first.erase(count, 1);
-                    first.insert(i, 1, temp[0]);
-                }
-            }
-            rearrangements(first, second, count+1);
+    // count keeps track of how deep we are in the recurrsion
+
+    // case that prints when weve hit bottom case 
+    if (count == first.size() - 1) {
+        cout << first + second << endl;
+    }
+    else {
+        for (int i = count; i <= first.size()-1; i++) {
+            swap(first[count], first[i]);
+            rearrangements(first, second, count + 1);
+            swap(first[count], first[i]);
         }
     }
 }
 
-int main()
-{
-    int count = 0;
-    rearrangements("cats", "fish",count);
+int main() {
+    rearrangements("CAT","MAN", 0);
+    return 0;
 }
-
